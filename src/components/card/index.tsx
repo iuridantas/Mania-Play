89472f10660b9837a39c03ea5ca8c6f7e98489fc
iuridantas <uri.dantas@hotmail.com>
2023-07-13 20:52:57 -0300

    feat: removed video

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import { CardSession } from './style';
 
 const modelsPhoto = [
@@ -31,55 +30,13 @@ const modelsPhoto = [
   },
 ];
 
-const modelsVideo = [
-  {
-    video: 'img/10.mp4',
-  },
-  {
-    video: 'img/11.mp4',
-  },
-  {
-    video: 'img/17.mp4',
-  },
-];
-
 export function CardModels() {
-  useEffect(() => {
-    function handleClick(event: MouseEvent) {
-      const videoId = (event.target as HTMLElement).id;
-      const video = document.getElementById(videoId) as HTMLMediaElement | null;
-
-      if (video !== null && video.paused) {
-        video.play();
-        video.classList.add('playing');
-      } else {
-        video?.pause();
-        video?.classList.remove('playing');
-      }
-    }
-
-    const videoElements = document.querySelectorAll('video');
-    videoElements.forEach((video) => {
-      video.addEventListener('click', handleClick);
-    });
-
-    return () => {
-      videoElements.forEach((video) => {
-        video.removeEventListener('click', handleClick);
-      });
-    };
-  }, []);
 
   return (
     <section aria-label="card dos modelos de expositores">
       {modelsPhoto.map((photo, index) => (
         <CardSession key={index} aria-label="card dos modelos de expositores">
           <img src={photo.photo} alt="Modelos de expositores" />
-        </CardSession>
-      ))}
-      {modelsVideo.map((video, index) => (
-        <CardSession key={index} aria-label="card dos modelos de expositores">
-          <video id={`video-${index}`} src={video.video} muted></video>
         </CardSession>
       ))}
     </section>
